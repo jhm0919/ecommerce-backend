cab4224aec027ab3126d5b3f6b7a4deefea15633
jhm0919 <90040776+jhm0919@users.noreply.github.com>
2026-04-29 13:34:41 +0900

    feat: Product 엔티티 추가
    생성 및 기본 필드

---
+++ management/src/main/java/com/team23/management/domain/product/Product.java
@@ -0,0 +1,57 @@
+package com.team23.management.domain.product;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Category category;
+
+    @Column(nullable = false)
+    private int basePrice;
+
+    @Lob
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ProductStatus status;
+
+    @Column(nullable = false)
+    private Long sellerId;
+
+    public static Product create(
+            String name, Category category, int basePrice,
+            String description, Long sellerId
+    ) {
+        validateName(name);
+        Product product = new Product();
+        product.name = name;
+        product.category = category;
+        product.basePrice = basePrice;
+        product.description = description;
+        product.sellerId = sellerId;
+        product.status = ProductStatus.ON_SALE;
+        return product;
+    }
+
+    public static void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("상품명은 필수입니다.");
+        }
+    }
+}
