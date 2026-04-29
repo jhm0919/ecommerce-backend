61bb811e21362a72d5cfa5b5d9c98417d2b628c0
jhm0919 <90040776+jhm0919@users.noreply.github.com>
2026-04-29 18:42:08 +0900

    feat: 재고 추가 기능 구현

---
+++ management/src/test/java/com/team23/management/domain/Stock/StockTest.java
@@ -0,0 +1,39 @@
+package com.team23.management.domain.Stock;
+
+import com.team23.management.domain.Sku.Sku;
+import com.team23.management.domain.Sku.SkuOptionInput;
+import com.team23.management.domain.Sku.SkuOptionValue;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StockTest {
+
+    @Test
+    @DisplayName("Stock 생성 시 quantity가 저장된다")
+    void createValidInput() {
+        Stock stock = Stock.create(1L, 1);
+
+        assertThat(stock.getQuantity()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("초기 quantity가 음수면 생성 실패")
+    void createQuantityMinusThrows() {
+        assertThatThrownBy(() ->
+                Stock.create(1L, -1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("수량");
+    }
+
+
+//    @DisplayName("decrease 시 quantity가 감소한다")
+//    @DisplayName("재고보다 많이 차감하려 하면 실패")
+//    @DisplayName("increase 시 quantity가 증가한다")
+}
