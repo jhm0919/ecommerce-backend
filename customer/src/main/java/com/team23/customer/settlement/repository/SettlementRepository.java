09fbb7bed51c85b34c45e26d7e7593a3a67c5e62
jhm0919 <90040776+jhm0919@users.noreply.github.com>
2026-05-12 13:30:10 +0900

    refactor: SettlementRepository 수정

--- customer/src/main/java/com/team23/customer/settlement/repository/SettlementRepository.java
+++ customer/src/main/java/com/team23/customer/settlement/repository/SettlementRepository.java
@@ -2,6 +2,8 @@ package com.team23.customer.settlement.repository;
 
 import com.team23.customer.settlement.domain.Settlement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +11,8 @@ import java.util.Set;
 
 public interface SettlementRepository extends JpaRepository<Settlement, Long> {
     // 이미 정산된 orderId 목록
-    Set<Long> findOrderIdsBySettledMonth(String settledMonth);
+    @Query("SELECT s.orderId FROM Settlement s WHERE s.settledMonth = :month")
+    Set<Long> findOrderIdsBySettledMonth(@Param("month") String settledMonth);
 
     // 월별 정산 내역 조회
     List<Settlement> findBySettledMonth(String settledMonth);
