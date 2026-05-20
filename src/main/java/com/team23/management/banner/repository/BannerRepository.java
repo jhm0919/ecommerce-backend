f809ab86b4292eeef37816501b030c4c1a3c46f9
jhm0919 <90040776+jhm0919@users.noreply.github.com>
2026-05-20 17:40:47 +0900

    feat: BannerRepository 추가 (#ADM-CNT-002-1)

---
+++ src/main/java/com/team23/management/banner/repository/BannerRepository.java
@@ -0,0 +1,8 @@
+package com.team23.management.banner.repository;
+
+import com.team23.management.banner.domain.Banner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BannerRepository extends JpaRepository<Banner, Long> {
+    boolean existsByDisplayOrder(int displayOrder);
+}
